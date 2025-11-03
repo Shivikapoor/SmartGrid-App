ee230bd929ac61ecd71422f2d04e43f2b1905cf9
Shivanshi Kapoor <94197335+Shivikapoor@users.noreply.github.com>
2025-11-04 01:48:13 +0530

    Configure app to run on specified port

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def predict():
         return jsonify({'status':'error','message': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
